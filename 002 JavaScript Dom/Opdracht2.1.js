0c921cf266a1b84e7a2598a5519d93c16c2d486c
DamianCameron <damiancameron99@gmail.com>
2020-01-30 14:24:12 +0100

    002 JavaScript Dom Opdracht 2.1 Gemaakt

---
+++ 002 JavaScript Dom/Opdracht2.1.js
@@ -0,0 +1,5 @@
+document.getElementById("paginatitel").innerText = "Dit is de paginatitel.";
+document.getElementById("subkop").innerText = "Dit is de subkop";
+document.getElementById("Rick").src = "img/Rick.png";
+document.getElementById("Morty").src = "img/Morty.jpg";
+
